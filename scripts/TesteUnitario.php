6a1ffdfab0b3a2e01c7d1053fc958c71ee218346
Airton <airtonmfilho@hotmail.com>
2016-09-29 20:27:11 -0300

    Refatorando teste unitario

--- scripts/TesteUnitario.php
+++ scripts/TesteUnitario.php
@@ -12,8 +12,16 @@ class TesteUnitario
 
 	function __construct()
 	{
+		self::testaMemoriaRam();
+	}
+
+	/**
+	 * Chama e testa todos os metodos da classe MemoriaRam
+	*/
+	public function testaMemoriaRam()
+	{
 		$this->MemoriaRam = new MemoriaRam();
-		// print_r($this->MemoriaRam->possuiEspacoNaMemoria());
+		print_r($this->MemoriaRam->possuiEspacoNaMemoria());
 		print_r($this->MemoriaRam->recebeComando([2,3,14,-1]));
 	}
 }
